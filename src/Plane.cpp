7a4b39b78b24136074a487cbbfdb615406490fa4
Duncan Burggraf <dcburggraf@gmail.com>
2016-06-05 22:09:56 -0700

    sketched Plane implementation

---
+++ src/Plane.cpp
@@ -0,0 +1,60 @@
+
+#include <raytracer.h>
+#include <cassert>
+#include <cmath>
+
+using namespace raytracer;
+
+/*
+	class Plane : public BasicBody {
+		private:
+		protected:
+		public:
+			Plane(const Vector & position, const Vector & normal);
+
+			virtual Vector getNormal(const Vector * p) const;
+			virtual double getDistance(const Ray * r) const;
+	};
+ */
+
+
+
+Plane::Plane(const Vector & position, const Vector & normal) {
+	setPosition(position);
+	z_axis = normal;
+}
+
+
+
+Vector Plane::getNormal(const Vector & p) const {
+	return z_axis;
+}
+
+
+
+double getDistance(const Ray & r) const {
+
+	Vector p = r.p; p.subtract(position);
+	double speed = z_axis.dot(r.v), distance;
+
+	if (speed < DIV_LIMIT) {
+		return -1.0;
+	}
+
+	distance = p.dot() / speed;
+
+	if (distance < DIV_LIMIT) {
+		return -1.0;
+	}
+
+	else {
+		return distance;
+	}
+
+}
+
+
+
+
+
+
